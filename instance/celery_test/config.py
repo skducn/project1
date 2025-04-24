e5b81b01ff0716f09c36e03ff00f23c2f031c036
skducn <skducn@163.com>
2025-04-24 15:54:33 +0800

    DESKTOP-DO7AEPU_20250424_5:5432

--- instance/celery_test/config.py
+++ instance/celery_test/config.py
@@ -9,18 +9,18 @@ filename = ['celeryPO.user_task', 'celeryPO.order_task',
 beat_schedule = {
     'task1': {
         'task': 'celeryPO.runStock.run',
-        'schedule': {'hour': 14, 'minute': 28},  # 指定时间为每天23点0分
+        'schedule': {'hour': 15, 'minute': 47},  # 指定时间为每天23点0分
         'args': ("4-22", "4-23")
     },
     'task2': {
         'task': 'celeryPO.crawal_today_sh.genData',
-        'schedule': {'hour': 15, 'minute': 16},  # 指定时间为每天13点0分
+        'schedule': {'hour': 15, 'minute': 51},  # 指定时间为每天13点0分
         'args': ()
     },
     'task3': {
         'name': 'celeryPO.add_task',
         'task': 'celeryPO.add_task.add',
-        'schedule': {'hour': 15, 'minute': 30},  # 指定时间为每天13点0分
+        'schedule': {'hour': 15, 'minute': 44},  # 指定时间为每天13点0分
         'args': (11, 44)
     }
 }
