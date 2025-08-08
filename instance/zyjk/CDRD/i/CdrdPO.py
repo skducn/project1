fec874f4fa5658793679fe2d71921023cc65d0ae
skducn <7873888+skducn@user.noreply.gitee.com>
2025-08-08 17:38:25 +0800

    fromMac

--- instance/zyjk/CDRD/i/CdrdPO.py
+++ instance/zyjk/CDRD/i/CdrdPO.py
@@ -12,7 +12,8 @@ private_key = '5034280c2b74c37289beaaa7edeed5d8fc7e9df12cf95db2800cc4b007e168e3'
 # *****************************************************************
 
 import subprocess, requests, json, os
-
+from PO.DataPO import *
+Data_PO = DataPO()
 
 class CdrdPO():
 
@@ -49,7 +50,6 @@ class CdrdPO():
             print(f"登录请求失败: {e}")
             return None
 
-
     def getDepartment(self):
 
         # 获取科室列表
@@ -79,7 +79,7 @@ class CdrdPO():
 
     def crtUser(self, varQty):
 
-        # 创建用户
+        # 批量创建用户
         # curl -X POST -H  "Accept:*/*" -H  "Authorization:eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoyNiwidXNlcl9rZXkiOiJlNGY0NzM4My01YWFjLTRlYmQtYmNmMS01ZThmZDA1YWQ2MTciLCJleHBpcmF0aW9uIjoiMzAiLCJ1c2VybmFtZSI6ImppbmhhbyJ9.o-8aGdZGBVPIRnXtotU4wPnr6T5SgqCogDRZ1CbTbcGA_IajxRRKjRkDlZpuUTjX4Sj8Pu0LSqK33XpPmP32NA" -H  "Content-Type:application/json" -d "{\"userId\":\"\",\"deptId\":\"\",\"userName\":\"lisi\",\"nickName\":\"lisi\",\"password\":\"\",\"phonenumber\":\"13636371322\",\"email\":\"yoyo@163.com.cn\",\"sex\":\"0\",\"status\":\"0\",\"remark\":\"我是哟哟\",\"postIds\":[],\"roleIds\":[6],\"departmentId\":31,\"departmentName\":\"科室\",\"departmentCode\":\"010\",\"jobNum\":\"3001\"}" "http://192.168.0.243:39210/system/user"
         #  -d "{\"userId\":\"\",\"deptId\":\"\",\"userName\":\"lisi\",\"nickName\":\"lisi\",\"password\":\"\",\"phonenumber\":\"13636371322\",
         #  \"email\":\"yoyo@163.com.cn\",\"sex\":\"0\",\"status\":\"0\",\"remark\":\"我是哟哟\",\"postIds\":[],\"roleIds\":[6],\"departmentId\":31,
@@ -90,24 +90,28 @@ class CdrdPO():
             'Authorization': self.token,
             'Content-Type': 'application/json'
         }
-        for _ in range(varQty):
 
-            ???
+        for i in range(varQty):
+
+            varUserName = "tester" + str(i)
+            varPhone = Data_PO.getPhone()
+            varEmail = varPhone + "@163.com"
+            varJob = 100000 + i
             data = {
-                "userName": "vivi12",
-                "nickName": "vivi12",
+                "userName": varUserName,
+                "nickName": varUserName,
                 "password": "",
-                "phonenumber": "11286371724",
-                "email": "vivi5@16921768.com.cn",
+                "phonenumber": varPhone,
+                "email": varEmail,
                 "sex": "1",
                 "status": "0",
                 "remark": "我是买手",
-                "roleIds": [6],
+                "roleIds": [6],   # sys_role中 role_id=6 测试工程师
                 "postIds": [],
-                "departmentId": "31",
-                "departmentName": "科室",
-                "departmentCode": "010",
-                "jobNum": "30112",
+                "departmentId": "31",   # sys_department中 department_id=31
+                "departmentName": "科室",  # sys_department中 department_name=科室
+                "departmentCode": "010",  # # sys_department中 department_code=010
+                "jobNum": varJob,
                 "pwdUpdateTime": "2033-12-12",
                 "pwdNextUpdateTime": "2033-12-12"
             }
@@ -127,3 +131,42 @@ class CdrdPO():
                 print(f"Response content: {response.text}")
                 return None
 
+
+    def getPatientInfoPage(self):
+
+        # 获取科室列表
+        url = self.ipPort + "/cdrdPatientInfo/getPatientInfoPage"
+        headers = {
+            'Accept': '*/*',
+            'Authorization': self.token,
+            'Content-Type': 'application/json'
+        }
+
+        data = {
+            "current":1,
+            "size":10,
+            "endPatientVisitInTime":"",  # 结束就诊日期
+            "patientDiagCode":"",  # ICD10编码
+            "patientDiagName":"",  # 患者诊断
+            "patientName":"",  # 患者姓名
+            "patientOutHospitalDiag":"",  # 出院诊断
+            "patientVisitDiag":"",  # 就诊诊断
+            "startPatientVisitInTime":""  # 开始就诊日期
+        }
+
+        try:
+            response = requests.post(url, headers=headers, json=data)
+            response.raise_for_status()  # 检查HTTP错误
+            d_r = response.json()
+            # print(d_r)
+            if d_r['code'] == 200:
+                return d_r['data']
+            else:
+                return d_r
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
+        except json.JSONDecodeError as e:
+            print(f"JSON decode error: {e}")
+            print(f"Response content: {response.text}")
+            return None
