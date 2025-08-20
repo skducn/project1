1c94f0e2fd7a19a9c79453c3f68fb9bd29d9660c
skducn <7873888+skducn@user.noreply.gitee.com>
2025-08-20 09:24:21 +0800

    fromMac

--- instance/playwright1/test.py
+++ instance/playwright1/test.py
@@ -10,3 +10,9 @@
 
 # *****************************************************************
 
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
