5d5182ed2c9abfbef1fd94b2463ffa6c6036531d
skducn <7873888+skducn@user.noreply.gitee.com>
2025-11-27 17:06:03 +0800

    fromMac

--- instance/stock/东方财富网/main.py
+++ instance/stock/东方财富网/main.py
@@ -29,6 +29,17 @@ from PO.OpenpyxlPO import *
 
 
 def xx(l_data, l_code):
+    # lcode = ['000025']
+    # 特殊中文名处理，如 "特', '力Ａ"中间有空格
+    # 获取，特', '力Ａ 000025的索引，删除后续个元素
+    for code in l_code:
+        if code in l_data:
+            index = l_data.index(code)
+            new_index = index + 1
+            l_data.pop(new_index)
+    return l_data
+
+def xxx(l_data, l_code):
     # lcode = ['000997','000992']
     # 特殊中文名处理，如 "新 大 陆"中间有空格
     # 获取，新大陆000997 的索引，删除后续2个元素
@@ -39,7 +50,6 @@ def xx(l_data, l_code):
             l_data.pop(new_index)
             l_data.pop(new_index)
     return l_data
-
 def main(d_url):
 
     sum = ''
@@ -47,10 +57,6 @@ def main(d_url):
         s = ''
         sign = 0
         l_l_data = []
-        # pathFile = "/Users/linghuchong/Downloads/" + str(k) + ".xlsx"
-        # NewexcelPO(pathFile)
-        # Openpyxl_PO = OpenpyxlPO(pathFile)
-        # Openpyxl_PO.appendRows([['序号','代码','名称','相关链接1','相关链接2','相关链接3','最新价','涨跌幅','涨跌额','成交量(手)','成交额','振幅','最高','最低','今开','昨收','量比','换手率','市盈率(动态)','市净率']])
 
         Web_PO = WebPO("chrome")
         Web_PO.openURL(v)
@@ -68,37 +74,40 @@ def main(d_url):
             l_data = s_data.replace('\n', ' ').split()
 
             # 特殊中文名处理，如 "新 大 陆"中间有空格
-            l_data = xx(l_data, ['000997','002186','002095','002183','002264'])
+            l_data = xx(l_data, ['000025','000528'])
+            l_data = xxx(l_data, ['000997','002186','002095','002183','002264','002209','002029','000735','002136'])
 
             # 每页20个票
             # ([['序号','代码','名称','相关链接1','相关链接2','相关链接3','最新价','涨跌幅','涨跌额','成交量(手)','成交额','振幅','最高','最低','今开','昨收','量比','换手率','市盈率(动态)','市净率']])
             for i in range(0, len(l_data), 20):
                 l_l_data.append(l_data[i:i + 20])
 
-            # print(91, l_l_data)
+            print(91, l_l_data)
             for l_data in l_l_data:
                 try:
-                    # 5,判断涨跌幅 2% - 5%区间
+                    # 判断条件：
                     l_data_7 = float(l_data[7].replace("%", ''))
+                    # 涨跌幅 3% - 15% 区间
                     # 代码小于 688000
-                    # 价格在区间[10, 30]
-                    # (最高价 -（最高价-最低价） * 0.35) > 最新价
-                    # (最新价 > (最高价 -（最高价-最低价） * 0.5),
-                    # 最低 >= (金开 - 金开 * 0.01)
-                    if l_data_7 > 4.5:
+                    # 最新价（收盘）在区间 [10, 60]
+                    # 上引线够长，（最高价 -（最高价 - 最低价）*0。3） > 最新价 ，，   收盘, （最高价 + 最低价）/2 > 最新价（收盘） (14.77 + 13.68)/2 > 14.40
+                    # (((float(l_data[12]) + float(l_data[14])) /2) > float(l_data[6])) and \
+                    # 阳线实体适中，(最新价(收盘) > (最高价 -（最高价-最低价） * 0.6),  42 -（42-37.35）*0.6 = 39.21
+                    # 下引线够短，最低 >= (今开 - 今开 * 0.02)  37.5 - 0.37 = 37.13
+                    if l_data_7 > 15:
                         ...
-                    elif (l_data_7 >= 2.5 and l_data_7 <= 4.5) :
-                        if int(l_data[1]) < 688000 and (float(l_data[6]) > 10 and float(l_data[6]) <= 30) and \
-                            ((float(l_data[12]) - (float(l_data[12]) - float(l_data[13])) * 0.35) > float(l_data[6])) and \
-                            (float(l_data[6]) > (float(l_data[12]) - (float(l_data[12]) - float(l_data[13])) * 0.5)) and \
-                            (float(l_data[13]) > (float(l_data[14]) - (float(l_data[14]) * 0.01))):
+                    elif (l_data_7 > 3 and l_data_7 <= 15) :
+                        if int(l_data[1]) < 688000 and (float(l_data[6]) > 10 and float(l_data[6]) <= 60) and \
+                            ((float(l_data[12]) - (float(l_data[12]) - float(l_data[13])) * 0.3) > float(l_data[6])) and \
+                            (float(l_data[6]) > (float(l_data[12]) - (float(l_data[12]) - float(l_data[13])) * 0.6)) and \
+                            (float(l_data[13]) > (float(l_data[14]) - (float(l_data[14]) * 0.02))):
                             s = l_data[1] + "," + s
                             # print(s)
                     else:
                         sign = 1
                         break
                 except:
-                    print(120, l_data)
+                    print(109, l_data)
                     sys.exit(0)
             l_l_data = []
 
@@ -114,14 +123,11 @@ def main(d_url):
     # 获取当天日期，如1118
     varDate = Time_PO.getMonth() + Time_PO.getDay()
     varFile = "/Users/linghuchong/Desktop/stock/all_" + str(varDate) + ".txt"
-    # varFile = "/Users/linghuchong/Desktop/stock/" + str(k) + str(varDate) + ".txt"
     with open(varFile, "w", encoding="utf-8") as f:
         f.write(sum)
 
 
 
-# 创业板
-# varUrl = "https://quote.eastmoney.com/center/gridlist.html#gem_board"
 # # 上证A股，注册制
 # varUrl = "https://quote.eastmoney.com/center/gridlist.html#sh_a_board_zcz"
 # # 上证A股，核准制
@@ -129,8 +135,6 @@ def main(d_url):
 # # 上证A股(全部)
 # varUrl = "https://quote.eastmoney.com/center/gridlist.html#sh_a_board"
 
-# d_url = {
-#     "创业板": "https://quote.eastmoney.com/center/gridlist.html#gem_board"}
 
 d_url = {
     "创业板": "https://quote.eastmoney.com/center/gridlist.html#gem_board",
@@ -140,4 +144,3 @@ d_url = {
 
 main(d_url)
 
-# 002438,001210,300218,301059,002824,001336,001311,300522,300446,001368,300755,001278,603217,605296,603153,603159,600475,300218,301059,300522,300446,300755,
